8b9b65fe272c057881ec53b5d85e666a8c4fe156
Maycon Prata da Conceicao (GOVBR - DDP) <Maycon.Conceicao@cetil.com.br>
2020-01-29 13:28:17 -0300

    conceitos básicos express

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 
+const express = require('express');
 
+const app = express();
 
+/**
+ * Pegando parametros através do queryParams
+ * Exemplo: http://localhost:3000/?name=Maycon
+ */
+
+app.get('/', (req, res)=>{
+    return res.send(`Bem-Vindo, ${req.query.name}`);
+});
+
+/**
+ * Pegando parametros vindo da url
+ * Exemplo: http://localhost:3000/nome/Maycon
+ */
+
+app.get('/nome/:name', (req, res)=>{
+    return res.send(`Bem-Vindo, ${req.params.name}`);
+});
+
+/**
+ * retornando json com o nome vindo da url
+ */
+
+app.get('/teste/:name2', (req, res)=>{
+    return res.json( {
+        message: `Bem vindo, ${req.params.name2}`
+    });
+});
+
+app.listen(3000);
